a746cf74ebbdb2ac771c4a228da19d015982fb25
briansimiyuj <briansimiyuj@gmail.com>
2020-12-01 14:30:04 +0300

    DOM Elements

---
+++ script.js
@@ -0,0 +1,14 @@
+const submitBTN = document.querySelector(".submitBTN")
+
+const song = document.querySelector(".song")
+
+const urlName = document.querySelector(".urlName")
+
+const url = document.querySelector(".url")
+
+const songName = document.querySelector(".songName")
+
+console.log(submitBTN, songName, urlName, url, song)
+
+
+
