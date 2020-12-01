5994c2801ac7b1c01710ea97f07ae39a7828eac1
briansimiyuj <briansimiyuj@gmail.com>
2020-12-01 15:36:59 +0300

    Appended Elements

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const urlName = document.querySelector(".urlName")
 
 const url = document.querySelector(".url")
 
+const container = document.querySelector(".container")
+
 const songName = document.querySelector(".songName")
 
-console.log(submitBTN, songName, urlName, url, song)
+console.log(submitBTN, songName, urlName, url, song, container)
 
 
 submitBTN.addEventListener('click', (e) =>{
@@ -20,6 +22,35 @@ submitBTN.addEventListener('click', (e) =>{
           alert('Please enter the song name')
 
           
+     }else{
+
+          const ul = document.createElement("ul")
+
+          const li = document.createElement("li")   
+
+          const label = document.createElement("label")
+
+          const a = document.createElement("a")   
+
+          const button = document.createElement("button")
+
+          label.textContent = songName.value
+
+          button.textContent = "Link"
+
+          a.href = urlName.value
+
+          a.appendChild(button)
+
+          li.appendChild(label)   
+
+          li.appendChild(a)   
+
+          ul.appendChild(li)
+
+          container.appendChild(ul)
+          
+
      }
 
 })
